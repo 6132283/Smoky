82f626e2a1476860bbe12212296adab1e8ead2ea
6132283 <alessandro.damico2@stud.unifi.it>
2020-10-23 01:40:41 +0200

    Added feature: searchbar working properly and fixed download button for csv

--- Smoky/static/js/demo/chart-area-demo.js
+++ Smoky/static/js/demo/chart-area-demo.js
@@ -2,15 +2,151 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-let obj = "ok";
 
-function SensorRow() {
+  function SensorRow() {
   this.num_of_graph_points = 0;
   this.id_lastread = 0;
 }
 
+  function download(data, strFileName, strMimeType) {
 
-function number_format(number, decimals, dec_point, thousands_sep) {
+		var self = window, // this script is only for browsers anyway...
+			defaultMime = "application/octet-stream", // this default mime also triggers iframe downloads
+			mimeType = strMimeType || defaultMime,
+			payload = data,
+			url = !strFileName && !strMimeType && payload,
+			anchor = document.createElement("a"),
+			toString = function(a){return String(a);},
+			myBlob = (self.Blob || self.MozBlob || self.WebKitBlob || toString),
+			fileName = strFileName || "download",
+			blob,
+			reader;
+			myBlob= myBlob.call ? myBlob.bind(self) : Blob ;
+
+		if(String(this)==="true"){ //reverse arguments, allowing download.bind(true, "text/xml", "export.xml") to act as a callback
+			payload=[payload, mimeType];
+			mimeType=payload[0];
+			payload=payload[1];
+		}
+
+
+		if(url && url.length< 2048){ // if no filename and no mime, assume a url was passed as the only argument
+			fileName = url.split("/").pop().split("?")[0];
+			anchor.href = url; // assign href prop to temp anchor
+		  	if(anchor.href.indexOf(url) !== -1){ // if the browser determines that it's a potentially valid url path:
+        		var ajax=new XMLHttpRequest();
+        		ajax.open( "GET", url, true);
+        		ajax.responseType = 'blob';
+        		ajax.onload= function(e){
+				  download(e.target.response, fileName, defaultMime);
+				};
+        		setTimeout(function(){ ajax.send();}, 0); // allows setting custom ajax headers using the return:
+			    return ajax;
+			} // end if valid url?
+		} // end if url?
+
+
+		//go ahead and download dataURLs right away
+		if(/^data\:[\w+\-]+\/[\w+\-]+[,;]/.test(payload)){
+
+			if(payload.length > (1024*1024*1.999) && myBlob !== toString ){
+				payload=dataUrlToBlob(payload);
+				mimeType=payload.type || defaultMime;
+			}else{
+				return navigator.msSaveBlob ?  // IE10 can't do a[download], only Blobs:
+					navigator.msSaveBlob(dataUrlToBlob(payload), fileName) :
+					saver(payload) ; // everyone else can save dataURLs un-processed
+			}
+
+		}//end if dataURL passed?
+
+		blob = payload instanceof myBlob ?
+			payload :
+			new myBlob([payload], {type: mimeType}) ;
+
+
+		function dataUrlToBlob(strUrl) {
+			var parts= strUrl.split(/[:;,]/),
+			type= parts[1],
+			decoder= parts[2] == "base64" ? atob : decodeURIComponent,
+			binData= decoder( parts.pop() ),
+			mx= binData.length,
+			i= 0,
+			uiArr= new Uint8Array(mx);
+
+			for(i;i<mx;++i) uiArr[i]= binData.charCodeAt(i);
+
+			return new myBlob([uiArr], {type: type});
+		 }
+
+		function saver(url, winMode){
+
+			if ('download' in anchor) { //html5 A[download]
+				anchor.href = url;
+				anchor.setAttribute("download", fileName);
+				anchor.className = "download-js-link";
+				anchor.innerHTML = "downloading...";
+				anchor.style.display = "none";
+				document.body.appendChild(anchor);
+				setTimeout(function() {
+					anchor.click();
+					document.body.removeChild(anchor);
+					if(winMode===true){setTimeout(function(){ self.URL.revokeObjectURL(anchor.href);}, 250 );}
+				}, 66);
+				return true;
+			}
+
+			// handle non-a[download] safari as best we can:
+			if(/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//.test(navigator.userAgent)) {
+				url=url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+				if(!window.open(url)){ // popup blocked, offer direct download:
+					if(confirm("Displaying New Document\n\nUse Save As... to download, then click back to return to this page.")){ location.href=url; }
+				}
+				return true;
+			}
+
+			//do iframe dataURL download (old ch+FF):
+			var f = document.createElement("iframe");
+			document.body.appendChild(f);
+
+			if(!winMode){ // force a mime that will download:
+				url="data:"+url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+			}
+			f.src=url;
+			setTimeout(function(){ document.body.removeChild(f); }, 333);
+
+		}//end saver
+
+
+
+
+		if (navigator.msSaveBlob) { // IE10+ : (has Blob, but not a[download] or URL)
+			return navigator.msSaveBlob(blob, fileName);
+		}
+
+		if(self.URL){ // simple fast and modern way using Blob and URL:
+			saver(self.URL.createObjectURL(blob), true);
+		}else{
+			// handle non-Blob()+non-URL browsers:
+			if(typeof blob === "string" || blob.constructor===toString ){
+				try{
+					return saver( "data:" +  mimeType   + ";base64,"  +  self.btoa(blob)  );
+				}catch(y){
+					return saver( "data:" +  mimeType   + "," + encodeURIComponent(blob)  );
+				}
+			}
+
+			// Blob but not URL support:
+			reader=new FileReader();
+			reader.onload=function(e){
+				saver(this.result);
+			};
+			reader.readAsDataURL(blob);
+		}
+		return true;
+	}; /* end download() */
+
+  function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
   number = (number + '').replace(',', '').replace(' ', '');
@@ -35,8 +171,7 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-
-function addData(chart, id_new, co_new, smoke_new, lpg_new, date_new, sensor_id, sensorRow) {
+  function addData(chart, id_new, co_new, smoke_new, lpg_new, date_new, sensor_id, sensorRow) {
 
     if(id_new > sensorRow.id_lastread) {
     console.log("Id:" + id_new + " co:" + co_new + " smoke: " + smoke_new +" gas: "+ lpg_new +" data: "+ date_new+" sensor ID: "+sensor_id);
@@ -60,43 +195,38 @@ function addData(chart, id_new, co_new, smoke_new, lpg_new, date_new, sensor_id,
 }
 }
 
-function askServer(chart, sensorIDToAsk, sensorRow){
-  $.ajax({
-  method: 'GET',
-  url: 'http://smokysmokysmoky.com/sensor_getlastdata.php',
-  dataType: 'json', //change the datatype to 'jsonp' works in most cases
-  data: {
+  function askServer(chart, sensorIDToAsk, sensorRow){
+    $.ajax({
+    method: 'GET',
+    url: 'http://smokysmokysmoky.com/sensor_getlastdata.php',
+    dataType: 'json', //change the datatype to 'jsonp' works in most cases
+    data: {
     sensorID: sensorIDToAsk,
-  },
+    },
 
-  success: (res) => {
-    addData(chart, res[0].ID, res[0].co2, res[0].smoke, res[0].gas, res[0].date, res[0].sensorID, sensorRow);
-    sensorRow.id_lastread = res[0].ID;
+    success: (res) => {
+      addData(chart, res[0].ID, res[0].co2, res[0].smoke, res[0].gas, res[0].date, res[0].sensorID, sensorRow);
+      sensorRow.id_lastread = res[0].ID;
 
   }
 
 });
 }
 
+  function getAllCsv(){
 
-function getAllCsv(){
+    $.ajax({
+    method: 'GET',
+    url: 'http://smokysmokysmoky.com/sensor.php',
+    dataType: 'json',
 
-  $.ajax({
-  method: 'GET',
-  url: 'http://smokysmokysmoky.com/sensor.php',
-  dataType: 'json',
-
-
-  success: (res) => {
-    var data = new Blob([convert(res)], {type : 'application/json'});
-    var url = window.URL.createObjectURL(data);
-    document.getElementById('download_link').href = url;
-  }
+    success: (res) => {
+      var data = new Blob([convert(res)], {type : 'application/json'});
+      download(data,"DatiSensore.csv");
+    }
 
 });
 }
-
-document.getElementById("download_link" ).click();
 
 	/**
 	 *
@@ -115,8 +245,7 @@ document.getElementById("download_link" ).click();
 	 * Copyright (c) 2018-2019 Martin Drapeau
 	 *
 	 */
-
-	var errorMissingSeparator = 'Missing separator option.',
+  let errorMissingSeparator = 'Missing separator option.',
 		  errorEmpty = 'JSON is empty.',
 		  errorEmptyHeader = 'Could not detect header. Ensure first row contains your column headers.',
 		  errorNotAnArray = 'Your JSON must be an array or an object.',
@@ -245,64 +374,58 @@ document.getElementById("download_link" ).click();
     return csv;
 	}
 
+  function switchOnOffDataset(chart, sensorAttribute){ //sensorNumber e' 1 o 2 con 2 dispositivi
 
+    let indexOfMeasurement;
 
+    switch(sensorAttribute) {
 
-function switchOnOffDataset(chart, sensorAttribute){ //sensorNumber e' 1 o 2 con 2 dispositivi
+      case "lpg":
+        indexOfMeasurement = 1;
+        break;
 
-  let indexOfMeasurement;
+      case "co":
+        indexOfMeasurement = 2;
+        break;
 
-  switch(sensorAttribute) {
-
-    case "lpg":
-      indexOfMeasurement = 1;
-      break;
-
-    case "co":
-      indexOfMeasurement = 2;
-      break;
-
-    case "smoke":
-      indexOfMeasurement = 0;
-      break;
+      case "smoke":
+        indexOfMeasurement = 0;
+        break;
+    }
+        chart.data.datasets[indexOfMeasurement].hidden = !chart.data.datasets[indexOfMeasurement].hidden;
   }
 
-        chart.data.datasets[indexOfMeasurement].hidden = !chart.data.datasets[indexOfMeasurement].hidden;
-
-}
-
-function switchOnOffDatasetCO(chart){
+  function switchOnOffDatasetCO(chart){
           chart.data.datasets[2].hidden = !chart.data.datasets[2].hidden;
-}
+  }
 
-function switchOnOffDatasetSmoke(chart){
+  function switchOnOffDatasetSmoke(chart){
           chart.data.datasets[0].hidden = !chart.data.datasets[0].hidden;
-}
+  }
 
-function switchOnOffDatasetLpg(chart){
+  function switchOnOffDatasetLpg(chart){
           chart.data.datasets[1].hidden = !chart.data.datasets[1].hidden;
 }
 
+  function createNewSensorGraph(graphName, sensorNumber) {
+    let node = document.getElementById("graph-row");
+    let clone = node.cloneNode(true);
+    clone.getElementsByClassName("chart-area")[0].innerHTML = "<canvas id = '" + graphName + "'></canvas>";
+    clone.style["display"] = "flex";
+    clone.getElementsByClassName("graph-title")[0].innerHTML = graphName;
 
-
-function createNewSensorGraph(graphName, sensorNumber) {
-  let node = document.getElementById("graph-row");
-  let clone = node.cloneNode(true);
-  clone.getElementsByClassName("chart-area")[0].innerHTML = "<canvas id = '" + graphName + "'></canvas>";
-  clone.style["display"] = "flex";
-  clone.getElementsByClassName("graph-title")[0].innerHTML = graphName;
-  clone.getElementsByClassName("sensors-text")[0].textContent ="HEY HEY";  //TODO modificare questo testo (con linea precedente)
-  document.getElementById("graph-body").appendChild(clone);
-  newChart1 = createGraph(graphName, sensorNumber); //dato un Id che do in input posso creare un grafico autoaggiornante
-  let random = Math.random().toString(36).substring(7);
-  coButtonid = graphName + "coButton" + random;
-  smokeButtonid = graphName + "smokeButton" + random;
-  gplButtonid = graphName + "gplButton" + random;
-  coButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ coButtonid +"'  onclick='switchOnOffDatasetCO(newChart1);'+' checked><label class='custom-control-label' for='"+coButtonid+"'>CO</label></div>";
-  smokeButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ smokeButtonid +"'  onclick='switchOnOffDatasetSmoke(newChart1);'+' checked><label class='custom-control-label' for='" + smokeButtonid + "'>Fumo</label></div>";
-  gplButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ gplButtonid +"'  onclick='switchOnOffDatasetLpg(newChart1);'+' checked><label class='custom-control-label' for='" + gplButtonid + "'>Gpl</label></div>";
-  clone.getElementsByClassName("bottoniera")[0].innerHTML = coButton + smokeButton + gplButton;
-
+    clone.getElementsByClassName("sensors-text")[0].textContent ="HEY HEY";  //TODO modificare questo testo (con linea precedente)
+    document.getElementById("graph-body").appendChild(clone);
+    newChart1 = createGraph(graphName, sensorNumber); //dato un Id che do in input posso creare un grafico autoaggiornante
+    let random = Math.random().toString(36).substring(7);
+    coButtonid = graphName + "coButton" + random;
+    smokeButtonid = graphName + "smokeButton" + random;
+    gplButtonid = graphName + "gplButton" + random;
+    coButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ coButtonid +"'  onclick='switchOnOffDatasetCO(newChart1);'+' checked><label class='custom-control-label' for='"+coButtonid+"'>CO</label></div>";
+    smokeButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ smokeButtonid +"'  onclick='switchOnOffDatasetSmoke(newChart1);'+' checked><label class='custom-control-label' for='" + smokeButtonid + "'>Fumo</label></div>";
+    gplButton = "<div class='custom-control custom-switch'><input type='checkbox' class='custom-control-input' id='"+ gplButtonid +"'  onclick='switchOnOffDatasetLpg(newChart1);'+' checked><label class='custom-control-label' for='" + gplButtonid + "'>Gpl</label></div>";
+    clone.getElementsByClassName("bottoniera")[0].innerHTML = coButton + smokeButton + gplButton;
+        clone.getElementsByClassName("chart-area")[0].classList.add(graphName);
 
     function createGraph(graphName, sensorToAsk) {
     // Area Chart Example
@@ -452,10 +575,21 @@ document.getElementById("newGraphButton").onclick = function(){
 };
 
 
-function prova1(){
-    var elmnt = document.getElementById("creators");
-  elmnt.scrollIntoView();
+
+
+
+function searchAndScroll(){
+    let stringa = document.getElementById("search-graph").value;
+    let elmnt = document.getElementsByClassName(stringa)[0];
+    elmnt.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
+
+
+$("#search-graph").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#searchbutton").click();
+    }
+});
 
 
 
